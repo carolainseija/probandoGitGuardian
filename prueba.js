1f089a96c8d036720312e522da95d94c082868b4
Carolain Seija <carolainsilva1@gmail.com>
2020-09-18 11:48:16 -0300

    agregue mas codigo

--- prueba.js
+++ prueba.js
@@ -3,3 +3,11 @@
  "mongodb+srv://carolain-seija:12345@cluster0.dk3mk.mongodb.net/canciones?retryWrites=true&w=majority",
  { useNewUrlParser: true }
  );
+
+/puerto del local host:
+app.listen(3000);
+console.log("http://localhost:3000");
+ No newline at end of file
+ 
+ //etc
+ 
